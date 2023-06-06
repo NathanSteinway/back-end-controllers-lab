b7b84cd01fa5b37c26d46a0d82bdd65b55928133
ganbari <steinway.nathan@gmail.com>
2023-06-06 14:25:33 -0500

    saves progress

---
+++ server/controller.js
@@ -0,0 +1,17 @@
+const homes = require('./db.json')
+const upcomingId = 4
+
+module.exports = {
+    getHouses: (req, res) => {
+        
+    },
+    deleteHouse: (req, res) => {
+
+    },
+    createHouse: (req, res) => {
+
+    },
+    updateHouse: (req, res) => {
+
+    }
+}
