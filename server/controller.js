00745edde99fe8e4815bc77d8e6602ef65b9db68
ganbari <steinway.nathan@gmail.com>
2023-06-06 15:25:14 -0500

    fixes port

--- server/controller.js
+++ server/controller.js
@@ -5,11 +5,13 @@ module.exports = {
     getHouses: (req, res) => {
         res.status(200).send(homes)
     },
+
     deleteHouse: (req, res) => {
-        let index = homes.findIndex(house => house.id === req.params.id)
+        let index = homes.findIndex(item => item.id === req.params.id)
         homes.splice(index, 1)
         res.status(200).send(homes)
     },
+
     createHouse: (req, res) => {
         let { address, price, imageURL } = req.body
         let newHouse = {
@@ -22,9 +24,10 @@ module.exports = {
         res.status(200).send(homes)
         upcomingId++
     },
+
     updateHouse: (req, res) => {
         let { id } = req.params
         let { type } = req.body
-        let index = homes.findIndex(house => house.id === id)
+        let index = homes.findIndex(item => item.id === id)
     }
 }
